38f7ffb7cd22902e0e4b3a78b1fdac1819b3fc24
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-15 16:38:50 -0300

    Criação do service

---
+++ exercico3/src/main/java/com/bn/exercico3/services/PedidoService.java
@@ -0,0 +1,36 @@
+package com.bn.exercico3.services;
+
+import com.bn.exercico3.models.PedidoModel;
+import com.bn.exercico3.repositories.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public List<PedidoModel> findAll(){
+        return pedidoRepository.findAll();
+    }
+
+    public PedidoModel criarPedido(PedidoModel pedidoModel){
+        return pedidoRepository.save(pedidoModel);
+    }
+
+    public void deletarPedido(Long id){
+        pedidoRepository.deleteById(id);
+    }
+
+    public PedidoModel buscarPedidoPorId(Long id){
+        return pedidoRepository.findById(id).get();
+    }
+
+    public PedidoModel atualizarPedido(Long id, PedidoModel pedidoModel){
+        PedidoModel newPedidoModel = pedidoRepository.findById(id).get();
+        return pedidoRepository.save(pedidoModel);
+    }
+}
